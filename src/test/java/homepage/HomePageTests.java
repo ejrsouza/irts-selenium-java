ac514e1a2d84daf4607098032365bd2256e98513
Elson Jr Souza <ribeirojunior93@gmail.com>
2020-06-03 22:40:49 -0300

    Criação de um Script de Teste com Maven e Selenium

---
+++ src/test/java/homepage/HomePageTests.java
@@ -0,0 +1,17 @@
+package homepage;
+
+import org.junit.jupiter.api.Test;
+
+import base.BaseTests;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class HomePageTests extends BaseTests {
+
+	@Test
+	public void testcontarProdutos_oitoProdutosDiferentes() {
+		carregarPaginaInicial();
+		assertThat(homePage.contarProdutos(), is(8));
+	}
+}
